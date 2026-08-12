be5b222e889cb9a631b819c2bbeb69d0de2ace98
alaraz1921 <alaraz1921@gmail.com>
2026-08-12 13:31:53 +0200

    Allow public donation settings reads

---
+++ supabase/migrations/20260812133000_public_donation_site_settings.sql
@@ -0,0 +1,6 @@
+drop policy if exists "Public can read visual site settings" on public.site_settings;
+
+create policy "Public can read visual site settings"
+on public.site_settings
+for select
+using (key in ('hero_logo_media', 'hero_media', 'city_media', 'donation_content'));
